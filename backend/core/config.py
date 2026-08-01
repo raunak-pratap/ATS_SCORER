0302e9532d7e408d60addf09a919db5617a8bef2
Raunak Pratap Singh <raunakthakur04@gmail.com>
2026-08-01 14:20:43 +0530

    set url

--- backend/core/config.py
+++ backend/core/config.py
@@ -17,11 +17,7 @@ APP_VERSION='1.0.0'
 APP_DESCRIPTION='analyse resumes against job description using nlp + ml'
 
 ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://appapppy-ktwxupi73vqhjzweksze9d.streamlit.app",
+    "https://atsscorer-eyxyrnmarvtgtc2gpgrkua.streamlit.app/"
 ]
 
 #file 
